ca457752ff55e98c57b801a3011e5c8abee0c1c9
MichaelCovert <michaeldcovert@gmail.com>
2018-12-01 14:02:46 -0800

    fix building collision boxes; add population from houses

---
+++ LudumDare43UnityProj/Assets/Scripts/Gameplay/Buildings/Housing.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.Gameplay;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Housing : MonoBehaviour {
+
+    public int populationSupply;
+
+    private void Start()
+    {
+        GameplayController.instance.maxPopulation += populationSupply;
+    }
+
+    private void OnDestroy()
+    {
+        if (GameplayController.instance != null)
+        {
+            GameplayController.instance.maxPopulation -= populationSupply;
+        }
+    }
+}
--- LudumDare43UnityProj/Assets/Scripts/Gameplay/GameplayController.cs
+++ LudumDare43UnityProj/Assets/Scripts/Gameplay/GameplayController.cs
@@ -4,12 +4,27 @@ namespace Assets.Scripts.Gameplay
 {
     public class GameplayController : MonoBehaviour
     {
+        public static GameplayController instance { get; private set; }
+
         [SerializeField] private PopulateWorld worldPopulator = null;
 
         public World.World World { get; private set; }
 
         public Resources.ResourceCollection currentResources { get; set; }
         public int maxPopulation { get; set; }
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        private void OnDestroy()
+        {
+            if(instance == this)
+            {
+                instance = null;
+            }
+        }
 
         public void Start()
         {
