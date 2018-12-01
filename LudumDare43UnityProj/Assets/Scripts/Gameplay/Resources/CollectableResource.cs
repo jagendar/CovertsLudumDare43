284164413892ee30f31673cecb217b857f3a1497
Christopher Covert <covertops5@gmail.com>
2018-12-01 18:41:21 -0500

    Add self-destruction to CollectableResources

--- LudumDare43UnityProj/Assets/Scripts/Gameplay/Resources/CollectableResource.cs
+++ LudumDare43UnityProj/Assets/Scripts/Gameplay/Resources/CollectableResource.cs
@@ -8,10 +8,22 @@ namespace Assets.Scripts.Gameplay.Resources
 
         [SerializeField] private ResourceType resourceType;
 
+        /// <summary>
+        /// If true, destroys its game object when amount reaches 0.
+        /// </summary>
+        [SerializeField] private bool selfDestroying = true;
+
         public uint Amount
         {
             get { return amount; }
-            set { amount = value; }
+            set
+            {
+                amount = value;
+                if (amount == 0 && selfDestroying)
+                {
+                    Destroy(gameObject);
+                }
+            }
         }
 
         public ResourceType ResourceType
