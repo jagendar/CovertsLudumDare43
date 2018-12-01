a8bbfc4ed69275140e8aeef20da05f0a45a59c54
Alex Covert <acovert@ucsc.edu>
2018-11-30 21:17:19 -0800

    Tiles have center at top

---
+++ LudumDare43UnityProj/Assets/Scripts/Gameplay/World/PopulateWorld.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.Gameplay.World;
+
+public class PopulateWorld : MonoBehaviour {
+    private World world;
+    [SerializeField] private Tile tile;
+
+    
+	void Start () {
+	}
+}
