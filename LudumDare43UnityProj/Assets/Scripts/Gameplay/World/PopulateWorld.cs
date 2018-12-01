f9c79a80d2a4b43091e0636c079198b91c066f36
Alex Covert <acovert@ucsc.edu>
2018-11-30 23:03:04 -0800

    Volcano in center

--- LudumDare43UnityProj/Assets/Scripts/Gameplay/World/PopulateWorld.cs
+++ LudumDare43UnityProj/Assets/Scripts/Gameplay/World/PopulateWorld.cs
@@ -7,9 +7,12 @@ public class PopulateWorld : MonoBehaviour {
     [SerializeField] private Tile waterTile, lavaTile, sandTile, dirtTile, grassTile;
 
     [SerializeField] private int worldSize;
+    [SerializeField] private int maxHeight; 
 
     private static Tile[,] worldArray;
     private World world;
+
+    private GameObject[,] tiles;
 
     private void Awake()
     {
@@ -18,16 +21,48 @@ public class PopulateWorld : MonoBehaviour {
     }
 
     void Start () {
-        Tile thisTile = dirtTile;
-        for(int x = 0; x < worldSize; x++)
+        Tile thisTile;
+        for (int x = 0; x < worldSize; x++)
         {
-            for(int z = 0; z < worldSize; z++)
+            for (int z = 0; z < worldSize; z++)
             {
-                thisTile.Height = 1;
+                if (worldSize - z < 3 || worldSize - x < 3 || x < 3 || z < 3)
+                {
+                    thisTile = sandTile;
+                }
+                else
+                {
+                    thisTile = dirtTile;
+                }
+
                 thisTile.transform.position = new Vector3(x, thisTile.Height, z);
                 world[x, z] = thisTile;
-                Instantiate(world[x, z]);
+                worldArray[x,z] = Instantiate(world[x, z]);
             }
         }
-	}
+
+        SetVolcanoHeights();
+    }
+
+    private void SetVolcanoHeights()
+    {
+        int heightSub = maxHeight;
+        for(int i = maxHeight; i >= 0; i-=2)
+        {
+            SetHeights(i, maxHeight - heightSub);
+            SetHeights(i - 1, maxHeight - heightSub);
+            heightSub--;
+        }
+    }
+
+    private void SetHeights(int volcanoLayer, int height)
+    {
+        for (int i = worldSize / 2 - volcanoLayer; i < worldSize / 2 + volcanoLayer; i++)
+        {
+            for(int j = worldSize / 2 - volcanoLayer; j < worldSize / 2 + volcanoLayer; j++)
+            {
+                worldArray[i, j].Height = height;
+            }
+        }
+    }
 }
