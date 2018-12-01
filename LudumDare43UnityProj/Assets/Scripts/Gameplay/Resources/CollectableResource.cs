e79fcefb0b08f3f686f705e089b28a93bb7c8f4a
Christopher Covert <covertops5@gmail.com>
2018-12-01 18:34:49 -0500

    Add resource amounts to rocks/trees

---
+++ LudumDare43UnityProj/Assets/Scripts/Gameplay/Resources/CollectableResource.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Gameplay.Resources
+{
+    public class CollectableResource : MonoBehaviour
+    {
+        [SerializeField] private uint amount;
+
+        [SerializeField] private ResourceType resourceType;
+
+        public uint Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+
+        public ResourceType ResourceType
+        {
+            get { return resourceType; }
+            set { resourceType = value; }
+        }
+    }
+}
