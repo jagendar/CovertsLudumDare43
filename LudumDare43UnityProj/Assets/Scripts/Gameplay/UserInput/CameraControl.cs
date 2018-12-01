ea8fe92c46c482cb35088d98218345781bd5da5a
MichaelCovert <michaeldcovert@gmail.com>
2018-12-01 10:01:30 -0800

    camera rotation

--- LudumDare43UnityProj/Assets/Scripts/Gameplay/UserInput/CameraControl.cs
+++ LudumDare43UnityProj/Assets/Scripts/Gameplay/UserInput/CameraControl.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,9 +11,15 @@ namespace Assets.Scripts.Gameplay.UserInput
         [SerializeField] Camera controlledCam;
         [SerializeField] Vector3 minCamera;
         [SerializeField] Vector3 maxCamera;
+
+        [SerializeField] float rotationTime;
+
+        private bool rotating;
         
         private void Update()
         {
+            if (rotating) return;
+
             Vector3 localMovement = new Vector2();
             if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
             {
@@ -48,8 +55,30 @@ namespace Assets.Scripts.Gameplay.UserInput
             RaycastHit hitInfo;
             if(Physics.Raycast(controlledCam.transform.position, controlledCam.transform.forward, out hitInfo, float.MaxValue, rotateLayer))
             {
+                Vector3 pos = transform.position;
+                Quaternion rot = transform.rotation;
                 transform.RotateAround(hitInfo.point, Vector3.up, degrees);
+                StartCoroutine(RotationLerp(pos, rot, transform.position, transform.rotation));
             }
+        }
+
+        private IEnumerator RotationLerp(Vector3 startPos, Quaternion startRot, Vector3 endPos, Quaternion endRot)
+        {
+            rotating = true;
+            for (float t = 0; t < rotationTime; t += Time.deltaTime)
+            {
+                RotationLerp(startPos, startRot, endPos, endRot, t / rotationTime);
+                yield return null;
+            }
+            RotationLerp(startPos, startRot, endPos, endRot, 1);
+
+            rotating = false;
+        }
+
+        private void RotationLerp(Vector3 pos1, Quaternion rot1, Vector3 pos2, Quaternion rot2, float percentage)
+        {
+            transform.position = Vector3.Lerp(pos1, pos2, percentage);
+            transform.rotation = Quaternion.Slerp(rot1, rot2, percentage);
         }
     }
 }
