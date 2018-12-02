4743780cfe515e84a3e9d07039ebf39eb0bd7234
MichaelCovert <michaeldcovert@gmail.com>
2018-12-02 13:44:22 -0800

    menu tweaking to make more cute

---
+++ LudumDare43UnityProj/Assets/PlayAnimPerSecond.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimPerSecond : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(AnimTrigger());
+	}
+	
+    IEnumerator AnimTrigger()
+    {
+        Animation anim = GetComponent<Animation>();
+        if(anim == null)
+        {
+            yield break;
+        }
+        while(true)
+        {
+            anim.Play();
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
