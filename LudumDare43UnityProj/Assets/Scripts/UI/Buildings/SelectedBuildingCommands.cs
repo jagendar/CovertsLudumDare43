b172c3cfa250401a45871f9bcfa8f18695cf79eb
Christopher Covert <covertops5@gmail.com>
2018-12-02 13:05:42 -0500

    Basic destroy button flow (doesn't destroy yet)

--- LudumDare43UnityProj/Assets/Scripts/UI/Buildings/SelectedBuildingCommands.cs
+++ LudumDare43UnityProj/Assets/Scripts/UI/Buildings/SelectedBuildingCommands.cs
@@ -9,6 +9,7 @@ namespace Assets.Scripts.UI.Buildings
         [SerializeField] private Button confirmYesButton;
         [SerializeField] private Button confirmNoButton;
         [SerializeField] private Button destroyButton;
+        [SerializeField] private RectTransform buttonsContainer;
 
         [SerializeField] private Vector2 offset = new Vector2(0, 0);
 
@@ -18,6 +19,9 @@ namespace Assets.Scripts.UI.Buildings
         public void Start()
         {
             controller = GameplayController.instance;
+            destroyButton.onClick.AddListener(OnDestroy);
+            confirmYesButton.onClick.AddListener(OnConfirmYes);
+            confirmNoButton.onClick.AddListener(OnConfirmNo);
         }
 
         public void Update()
@@ -26,7 +30,7 @@ namespace Assets.Scripts.UI.Buildings
             confirmYesButton.gameObject.SetActive(showConfirms);
             confirmNoButton.gameObject.SetActive(showConfirms);
 
-            bool showDestroy = controller.SelectedBuilding && needsConfirmation;
+            bool showDestroy = controller.SelectedBuilding && !needsConfirmation;
             destroyButton.gameObject.SetActive(showDestroy);
 
             if (!controller.SelectedBuilding) return;
@@ -34,7 +38,25 @@ namespace Assets.Scripts.UI.Buildings
             var targetPosition = controller.SelectedBuilding.transform.position;
             var targetScreenPosition = Camera.main.WorldToScreenPoint(targetPosition);
 
-            gameObject.transform.position = targetScreenPosition + new Vector3(offset.x, 0, offset.y);
+            gameObject.transform.position = targetScreenPosition + new Vector3(offset.x - buttonsContainer.rect.width / 2, offset.y, 0);
+        }
+
+        public void OnDestroy()
+        {
+            Debug.Log("OnDestroy");
+            needsConfirmation = true;
+        }
+
+        public void OnConfirmNo()
+        {
+            Debug.Log("OnConfirmNo");
+            needsConfirmation = false;
+        }
+
+        public void OnConfirmYes()
+        {
+            Debug.Log("OnConfirmYes");
+            needsConfirmation = false;
         }
     }
 }
