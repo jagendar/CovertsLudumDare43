7edfabc7fd6fdea5b52617e31c5a055d8c8c77cf
Alex Covert <acovert@ucsc.edu>
2018-12-01 23:31:43 -0800

    Main Menu & gameplay controller reference

---
+++ LudumDare43UnityProj/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    private void Start()
+    {
+        
+    }
+
+    public void OnClickPlay()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    
+
+}
