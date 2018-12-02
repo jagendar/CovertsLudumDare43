e8cf96c68eee872eecf66fd2542717f8b6c23248
MichaelCovert <michaeldcovert@gmail.com>
2018-12-01 22:32:12 -0800

    refactor volcano projectiles into a coroutine (useful for tracking start/end events)

--- LudumDare43UnityProj/Assets/Scripts/Gameplay/World/LavaShot.cs
+++ LudumDare43UnityProj/Assets/Scripts/Gameplay/World/LavaShot.cs
@@ -7,16 +7,28 @@ namespace Assets.Scripts.Gameplay.World
 {
     public class LavaShot : MonoBehaviour
     {
-        private Tile target;
+        [SerializeField] Material lavaMat;
 
         internal void Initialize(Tile tile)
         {
-            target = tile;    
+            StartCoroutine(MoveToTile(tile));
         }
 
-        private void Update()
+        private IEnumerator MoveToTile(Tile currentTarget)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, .1f);
+            Vector3 pos = transform.position;
+            const float translationTime = 2.5f;
+            for (float t = 0; t < translationTime; t += Time.deltaTime)
+            {
+                SlideLerp(pos, currentTarget.transform.position, t / translationTime);
+                yield return null;
+            }
+            SlideLerp(pos, currentTarget.transform.position, 1);
+        }
+
+        private void SlideLerp(Vector3 startPosition, Vector3 targetPosition, float v)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, v);
         }
     }
 }
