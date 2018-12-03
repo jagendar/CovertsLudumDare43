31e4672abcae4689ef688278d38be2b05c675dd3
Christopher Covert <covertops5@gmail.com>
2018-12-02 20:08:28 -0500

    Add resource highlighting while building

---
+++ LudumDare43UnityProj/Assets/Scripts/Gameplay/Resources/ResourceHighlight.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Gameplay.Resources
+{
+    public class ResourceHighlight : MonoBehaviour
+    {
+        [SerializeField] private GameObject highlightTemplate;
+
+        private GameObject highlightInstance;
+
+        public bool IsHighlighted { get; set; }
+
+        public void Awake()
+        {
+            highlightInstance = Instantiate(highlightTemplate, transform.position, Quaternion.identity);
+        }
+
+        public void Update()
+        {
+            highlightInstance.SetActive(IsHighlighted);
+        }
+    }
+}
